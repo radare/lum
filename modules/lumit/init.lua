44d60df8073150ab0d486697be8f05f659104130
radare <pancake@nopcode.org>
2011-12-19 12:32:21 +0100

    More work on lum

--- modules/lumit/init.lua
+++ modules/lumit/init.lua
@@ -27,8 +27,13 @@ end
 
 function Lumit.init (self, fn)
 	Lumit.CWD = process.cwd ()
+	Lumit.MAKE = process.env["MAKE"] or "make"
 	Lumit.CC = process.env["CC"] or "gcc -arch i386"
-	Lumit.LUA_DIR = process.env["LUA_DIR"] or ""
+	Lumit.LUVIT_DIR = process.env["LUVIT_DIR"] or ""
+	Lumit.LUA_DIR = process.env["LUA_DIR"]
+	if not Lumit.LUA_DIR and Lumit.LUVIT_DIR then
+		Lumit.LUA_DIR = Lumit.LUVIT_DIR.."/deps/luajit/src"
+	end
 	System.cmdstr ("uname -ms", function (err, os)
 		if err>0 then
 			Lumit.UNAME = "Unknown"
@@ -40,13 +45,17 @@ function Lumit.init (self, fn)
 end
 
 function Lumit.clean(self, nextfn)
-	System.cmd ("make clean", function (ret)
+	System.cmd (Lumit.MAKE.." clean", function (ret)
 		if nextfn then nextfn (self) end
 	end)
 end
 
 function Lumit.build_dep(self, pkg, nextfn)
 	local wrkdir = self.CWD.."/_build"
+	if FS.exists_sync ("./modules/"..pkg) then
+		p ("Already installed: "..pkg)
+		return
+	end
 	local db = DB.open ()
 	db:find (pkg, function (x)
 		if not x then 
@@ -54,23 +63,38 @@ function Lumit.build_dep(self, pkg, nextfn)
 			-- process.exit (1)
 			return
 		end
-		p ("pkg "..x.name .." found")
-		if x.type == "git" then
-			local cmd = "mkdir -p "..wrkdir.." ; git clone "..x.url.." "..wrkdir.."/"..x.name
-			p(cmd)
+		local wrkname = wrkdir.."/"..x.name
+		if FS.exists_sync (wrkname) then
+			p ("Already installed: "..pkg)
+			cmd =   "cd "..wrkdir.."/"..x.name.." ; "..
+				"lum && lum deploy '"..self.CWD.."'"
+			p (cmd)
 			System.cmd (cmd, function (cmd, err)
-				cmd =   "cd "..wrkdir.."/"..x.name.." ; "..
-					"lum && lum install "..self.CWD
-				p (cmd)
-				System.cmd (cmd, function (cmd, err)
-					p ("exit with "..err)
-				end)
+				p ("exit with "..err)
 			end)
-		elseif x.type == "dist" then
-			-- distribution tarball
-			p ("ERROR", "Unimplemented package type")
+			return
 		else
-			p ("ERROR", "Unknown package type")
+			if x.type == "git" then
+				local cmd = "mkdir -p "..wrkdir.." ; git clone "..x.url.." "..wrkdir.."/"..x.name
+				p(cmd)
+				System.cmd (cmd, function (cmd, err)
+					if (err>0) then
+						p ("Cannot clone '"..x.name.."' from "..x.url)
+						process.exit (1)
+					end
+					cmd =   "cd "..wrkdir.."/"..x.name.." ; "..
+						"lum && lum install "..self.CWD
+					p (cmd)
+					System.cmd (cmd, function (cmd, err)
+						p ("exit with "..err)
+					end)
+				end)
+			elseif x.type == "dist" then
+				-- distribution tarball
+				p ("ERROR", "Unimplemented package type")
+			else
+				p ("ERROR", "Unknown package type '"..x.type.."'")
+			end
 		end
 		if nextfn then nextfn () end
 	end)
@@ -78,20 +102,21 @@ end
 
 function Lumit.build(self, nextfn)
 	-- XXX: this must be luvit! lua5.x != luajit --
-	local path = self.LUA_DIR
-	if path == "" then 
-		path = "/usr/include"
-		if FS.exists_sync ("/usr/include/lua.h") then
-			path = "/usr/include"
-		elseif FS.exists_sync ("/opt/local/include/lua.h") then
-			path = "/opt/local/include"
-		elseif FS.exists_sync ("/usr/local/include/lua.h") then
-			path = "/usr/local/include" end
-	end
+--	local path = self.LUA_DIR
+--	if path == "" then 
+--		path = "/usr/include"
+--		if FS.exists_sync ("/usr/include/lua.h") then
+--			path = "/usr/include"
+--		elseif FS.exists_sync ("/opt/local/include/lua.h") then
+--			path = "/opt/local/include"
+--		elseif FS.exists_sync ("/usr/local/include/lua.h") then
+--			path = "/usr/local/include" end
+--	end
+	local path = self.LUVIT_DIR or self.LUA_DIR or ""
 	if not FS.exists_sync (path.."/lua.h") then
 		path = path.."/deps/luajit/src"
 		if not FS.exists_sync (path.."/lua.h") then
-			print ("ERROR: Cannot find lua.h in LUA_DIR ("..self.LUA_DIR..")")
+			print ("ERROR: Cannot find lua.h in LUVIT_DIR ("..self.LUVIT_DIR..")")
 			process.exit (1)
 		end
 	end
@@ -105,16 +130,20 @@ function Lumit.build(self, nextfn)
 		ldflags = "-shared -fPIC"
 	end
 
-	local cmd = 
-		" CC='"..self.CC.."'"..
-		" CFLAGS='"..cflags.."'"..
-		" LDFLAGS='"..ldflags.."'"..
-		" LUA_DIR='"..path.."' make"
-	p(cmd)
-	System.cmd (cmd, function (cmd, err)
-		print ("exit with "..err)
+	if FS.exists_sync ("Makefile") then
+		local cmd = 
+			" CC='"..self.CC.."'"..
+			" CFLAGS='"..cflags.."'"..
+			" LDFLAGS='"..ldflags.."'"..
+			" LUA_DIR='"..path.."' "..Lumit.MAKE
+		p(cmd)
+		System.cmd (cmd, function (cmd, err)
+			print ("exit with "..err)
+			if nextfn then nextfn (self) end
+		end)
+	else
 		if nextfn then nextfn (self) end
-	end)
+	end
 end
 
 function Lumit.deps(self, nextfn)
@@ -147,7 +176,11 @@ function Lumit.uninstall(self, pkg, nextfn)
 	end)
 end
 
-function Lumit.install(self, path, nextfn)
+function Lumit.install(self, pkg, nextfn)
+	self:build_dep (pkg, nextfn)
+end
+
+function Lumit.deploy(self, path, nextfn)
 	if not path then
 		p ("Missing argument")
 		return
@@ -159,10 +192,16 @@ function Lumit.install(self, path, nextfn)
 			"mkdir -p '"..path.."/modules/"..pkgname.."' && "..
 			"cp -f package.lua '"..path.."/modules/"..pkgname.."' && "..
 			"cp -f modules/"..pkgname.."/* '"..path.."/modules/"..pkgname.."'"
-		-- TODO: copy binaries ?
-		p (cmd)
-		System.cmd (cmd, function (err)
-			p ("Done with exit ".. err)
+		-- TODO: copy binaries using luvit-fsutils
+		p ("--->"..cmd)
+		System.cmd (cmd, function (cmd, err)
+			if err>0 then
+				p ("ERROR", "Installation failed for module "..pkgname)
+				-- TODO:  remove 
+				System.cmd ("rm -rf '"..path.."/modules/"..pkgname.."'", function (x)
+					process.exit (1)
+				end)
+			end
 			if nextfn then nextfn (err) end
 		end)
 	end)
